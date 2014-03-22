9080bba329009b669d15ef5ff535b9cf2bf131a4
Luca Venturini <dr.wen289@gmail.com>
2014-03-22 17:55:58 +0100

    New class that represent the model of comunication to the threads thtat perform download

---
+++ youtube-downloader/src/main/java/it/ventuland/ytd/YTDownloadThreadData.java
@@ -0,0 +1,11 @@
+package it.ventuland.ytd;
+
+import it.ventuland.ytd.ui.UrlList;
+
+public class YTDownloadThreadData {
+
+	public UrlList videoElementQueue;
+	public boolean isDebug;
+	public boolean forceNoDowload;
+	
+}
---
+++ youtube-downloader/src/main/java/it/ventuland/ytd/ui/VideoElement.java
@@ -0,0 +1,9 @@
+package it.ventuland.ytd.ui;
+
+public class VideoElement {
+
+	public String videoQuality;
+	public String videoFormat;
+	public String videoUrl;
+	public boolean is3D;
+}
